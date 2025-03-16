1a056607690dae1fc7889280bab2cc0857725bf8
Sadam Hussain <d_shussain@universalweather.net>
2025-03-15 20:27:09 -0400

    Added Auth and Security

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,16 @@
+import { config } from "dotenv";
+
+import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env.local" });
+
 export default {
   dialect: "postgresql",
   schema: "./utils/db/schema.ts",
   out: "./drizzle",
 
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
     connectionString: process.env.DATABASE_URL,
   },
 };
